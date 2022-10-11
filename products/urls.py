f01f9db9a9effca587da19cb8fe64d68c21892d4
Komilov185657 <komilov185657@gmail.com>
2022-10-11 21:38:56 +0500

    Update . de;ete add

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import add_prodect
+from .views import add_prodect, delete_product, update_product
 
 urlpatterns= [
     path('addproduct/', add_prodect, name = 'add_product'),
+    path('delete/<int:pk>', delete_product, name = 'delete'),
+    path('update/<int:pk>', update_product, name = 'update'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -5,8 +5,9 @@ import datetime
 # Create your views here.
 
 def add_prodect(request):
-    print("11111111111111111111111111111", request.user)
+    # print("11111111111111111111111111111", request.user)
     if request.user.is_superuser:
+        
         if request.method == "POST":
             try:
             
@@ -40,8 +41,61 @@ def add_prodect(request):
             except:
                 messages.warning(request, ("Error : There was an error adding the product. Please try again"),)
             return redirect('home')
-
+        
+             
         else:
             return render(request, template_name='add_product.html')
     else:
         return redirect('home')
+
+def delete_product(request, pk=None):
+    if request.user.is_superuser and pk:
+        if pk:
+            product = Product.objects.filter(pk = pk)[0]
+            if product:
+                Product.delete(product)
+                messages.success(request, ("Products delete successful"),)
+            else:
+                messages.warning(request, ("Not found pk, please try again"),)    
+                return redirect(request, ("Not found product, please try again"))
+        else:
+            messages.success(request, ("Not found pk, please try again"),)
+        return redirect('home')
+    else:
+        return redirect('home')
+
+def update_product(request, pk = None):
+    if request.user.is_superuser:
+        if request.method == "GET":
+            product = Product.objects.get(pk = pk)
+            return render(request, 'update_product.html', {"obj" : product})
+        elif request.method == "POST":
+            # try:
+            product = Product.objects.get(pk=pk)
+            if request.POST['title']:
+                product.title = request.POST['title']
+            if request.POST['bio']:
+                product.bio = request.POST['bio']
+            if request.POST['deff']:
+                product.definition = request.POST['deff']
+            if request.POST['price']:
+                product.price = request.POST['price']
+            if request.POST['count']:
+                product.count = request.POST['count']
+            if request.POST['dprice']:
+                product.delivery_price = request.POST['dprice']
+            if request.FILES.get('photo1'):
+                product.photo1 = request.FILES['photo1']
+            if request.FILES.get('photo2'):
+                product.photo2 = request.FILES.get('photo2')
+            if request.FILES.get('photo3'):
+                product.photo3 = request.FILES.get('photo3')
+            if request.FILES.get('photo4'):
+                product.photo4 = request.FILES.get('photo4')
+            
+            product.save()
+            #     messages.success(request, ("Success edited product"))
+            # except:
+            #     messages.warning(request, ("Error: edited product, please try again"))
+            return redirect('home')
+
